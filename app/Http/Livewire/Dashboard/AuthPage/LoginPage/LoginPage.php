489181206111eb6fb19b5ae85a7c9d41b4c2e78a
kingforks <cloudmyn46@gmail.com>
2023-10-09 09:32:21 +0800

    add localization

--- app/Http/Livewire/Dashboard/AuthPage/LoginPage/LoginPage.php
+++ app/Http/Livewire/Dashboard/AuthPage/LoginPage/LoginPage.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Dashboard\AuthPage\LoginPage;
 use App\Http\Livewire\Dashboard\_Dashboard;
 use App\Http\Livewire\Dashboard\DispatchType;
 use App\Models\User;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 
